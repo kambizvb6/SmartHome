d4aa39740e7c66fad3836e16688763f38da99380
Kambiz Dehghani <kambizvb6@gmail.com>
2024-01-22 22:17:52 -0600

    Initial Commit

---
+++ SmartHome/SmartHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeApp.swift
+//  SmartHome
+//
+//  Created by Kambiz on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
